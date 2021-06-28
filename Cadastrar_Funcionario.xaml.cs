9494413db4e93733096c3a70a2b603f028242fc8
Paulo Santos Silva <paulo252003@hotmail.com>
2021-06-28 13:39:32 -0400

    this.close cadastrar funcionario

--- Cadastrar_Funcionario.xaml.cs
+++ Cadastrar_Funcionario.xaml.cs
@@ -28,18 +28,21 @@ namespace Siscream
         {
             SubMenu_Gastos submenu_gastos = new SubMenu_Gastos();
             submenu_gastos.Show();
+            this.Close();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             SubMenu_Produtos submenu_produtos = new SubMenu_Produtos();
             submenu_produtos.Show();
+            this.Close();
         }
 
         private void btn_cadastros_Click(object sender, RoutedEventArgs e)
         {
             SubMenu_Cadastros submenu_cadastrar = new SubMenu_Cadastros();
             submenu_cadastrar.Show();
+            this.Close();
         }
 
         private void btn_close_Click(object sender, RoutedEventArgs e)
@@ -51,30 +54,35 @@ namespace Siscream
         {
             SubMenu_Vendas submenu_vendas = new SubMenu_Vendas();
             submenu_vendas.Show();
+            this.Close();
         }
 
         private void btn_cadastrar_Click_1(object sender, RoutedEventArgs e)
         {
             Popup_Cadastrar_Funcionario popup = new Popup_Cadastrar_Funcionario();
             popup.ShowDialog();
+            this.Close();
         }
 
         private void btn_cancelar_Click(object sender, RoutedEventArgs e)
         {
             Popup_Cancelar_Funcionario popup = new Popup_Cancelar_Funcionario();
             popup.ShowDialog();
+            this.Close();
         }
 
         private void btn_editar_Click(object sender, RoutedEventArgs e)
         {
             Popup_Editar_Funcionario popup = new Popup_Editar_Funcionario();
             popup.ShowDialog();
+            this.Close();
         }
 
         private void btn_demitir_Click(object sender, RoutedEventArgs e)
         {
             Popup_Demitir_Funcionario popup = new Popup_Demitir_Funcionario();
             popup.ShowDialog();
+            this.Close();
         }
 
         private void btn_produtos_Click(object sender, RoutedEventArgs e)
