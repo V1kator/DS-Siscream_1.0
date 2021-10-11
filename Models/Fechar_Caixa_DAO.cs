7b85b24c3c238d291d64d69e86da453ebb7a98ae
TM-Souza <thalliamichelle@gmail.com>
2021-10-11 16:48:10 -0300

    Models - DAO Fechar Caixa

---
+++ Models/Fechar_Caixa_DAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Siscream.Interfaces;
+
+
+namespace Siscream.Models
+{
+    class Fechar_Caixa_DAO : IDAO<Fechar_Caixa>
+    {
+        public void Delete(Fechar_Caixa t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fechar_Caixa GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Fechar_Caixa t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Fechar_Caixa> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Fechar_Caixa t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
