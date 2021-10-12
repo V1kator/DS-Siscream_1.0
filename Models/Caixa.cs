39e4803cc18ccbe592b68b903ae90b78c03020ce
TM-Souza <thalliamichelle@gmail.com>
2021-10-12 12:23:38 -0300

    Models - Caixa

---
+++ Models/Caixa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siscream.Models
+{
+    class Caixa
+    {
+        public string nome_func { get; set; }
+        public double saldo { get; set; }
+        public int senha { get; set; }
+        
+    }
+}
