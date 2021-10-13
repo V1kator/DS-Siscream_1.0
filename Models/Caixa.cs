0f3c47b80974469cb2bfaa3d65a63e9204599de2
TM-Souza <thalliamichelle@gmail.com>
2021-10-12 23:53:51 -0300

    Atualizações Caixa

--- Models/Caixa.cs
+++ Models/Caixa.cs
@@ -12,6 +12,16 @@ namespace Siscream.Models
         public double saldo { get; set; }
         public int senha { get; set; }
         public DateTime aberto { get; set; }
-        
+        public DateTime fechado { get; set; }
+        public double valorAbertura { get; set; }
+        public double suprimento { get; set; }
+        public double dinheiroCX { get; set; }
+        public double creditoCX { get; set; }
+        public double debitoCX { get; set; }
+        public double totalCX { get; set; }
+        public double valorRetirado { get; set; }
+        public string especif { get; set; }
+        public double saldoFinal { get; set; }
+
     }
 }
--- Models/FecharCaixaDAO.cs
+++ Models/FecharCaixaDAO.cs
@@ -25,17 +25,21 @@ namespace Siscream.Models
                 var query = conn.Query();
                 query.CommandText = "INSERT INTO tb_caixa (funcionario_caixa, caixa_aberto, caixa_fechado, valorAbertura_caixa," +
                 "suprimento, dinheiro_caixa, credito_caixa, debito_caixa, total_entrada, valor_retirado, especificacoes, saldofinal_caixa) " +
-                "VALUES (@nome_func, @aberto, @fechado, @valorAbertura, @suprimento, @dinheiro, @credito, @debito, @total, @valorRetirado, @descricao, @saldoFinal)";
+                "VALUES (@nome_func, @aberto, @fechado, @valorAbertura, @suprimento, @dinheiroCX, @creditoCX, @debitoCX, @totalCX, @valorRetirado, @especif @saldoFinal)";
 
                 query.Parameters.AddWithValue("@nome_func", t.nome_func);
                 query.Parameters.AddWithValue("@aberto", t.aberto);
-                query.Parameters.AddWithValue("", t);
-                query.Parameters.AddWithValue("", t);
-                query.Parameters.AddWithValue("", t);
-                query.Parameters.AddWithValue("", t);
-                query.Parameters.AddWithValue("", t);
-                query.Parameters.AddWithValue("", t);
-                query.Parameters.AddWithValue("", t);
+                query.Parameters.AddWithValue("@valorAbertura", t.valorAbertura);
+                query.Parameters.AddWithValue("@suprimento", t.suprimento);
+                query.Parameters.AddWithValue("@dinheiroCX", t.dinheiroCX);
+                query.Parameters.AddWithValue("@creditoCX", t.dinheiroCX);
+                query.Parameters.AddWithValue("@debitoCX", t.debitoCX);
+                query.Parameters.AddWithValue("@totalCX", t.totalCX);
+                query.Parameters.AddWithValue("@valorRetirado", t.valorRetirado);
+                query.Parameters.AddWithValue("@especif", t.especif);
+                query.Parameters.AddWithValue("@saldoFinal", t.saldoFinal);
+
+                var result = query.ExecuteNonQuery();
             }
             catch (Exception e)
             {
