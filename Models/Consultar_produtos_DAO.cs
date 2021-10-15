9e8dd02c26e887395bb2d57914071d703c1d1c42
Gabriel Henrique <ghlkoficial@gmail.com>
2021-10-15 19:58:24 -0300

    Consultar Produtos DAO - Update

--- Models/Consultar_produtos_DAO.cs
+++ Models/Consultar_produtos_DAO.cs
@@ -8,7 +8,31 @@ using Siscream.Interfaces;
 
 namespace Siscream.Models
 {
-    class Consultar_produtos_DAO
+    class Consultar_produtos_DAO : IDAO<Consultar_produtos>
     {
+        public void Delete(Consultar_produtos t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Consultar_produtos GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Consultar_produtos t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Consultar_produtos> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Consultar_produtos t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
