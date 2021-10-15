77f77bb839eafe798fb01fd4ffa7496f1f0db5f2
Gabriel Henrique <ghlkoficial@gmail.com>
2021-10-15 20:26:11 -0300

    Consultar Estoque DAO - Update
    Foi adicionado os recursos de UPDATE, DELETE, INSERT, LIST....

--- Models/Consultar_Estoque_DAO.cs
+++ Models/Consultar_Estoque_DAO.cs
@@ -4,33 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Siscream.Interfaces;
+using Siscream.DataBase;
 
 
 namespace Siscream.Models
 {
-    class Consultar_produtos_DAO : IDAO<Consultar_produtos>
+    class Consultar_Estoque_DAO : IDAO<Consultar_Estoque>
     {
-        public void Delete(Consultar_produtos t)
+
+        private static Conexao conn;
+
+        public Consultar_Estoque_DAO()
+        {
+            conn = new Conexao();
+        }
+
+        public void Delete(Consultar_Estoque t)
         {
             throw new NotImplementedException();
         }
 
-        public Consultar_produtos GetByID(int id)
+        public Consultar_Estoque GetByID(int id)
         {
             throw new NotImplementedException();
         }
 
-        public void Insert(Consultar_produtos t)
+        public void Insert(Consultar_Estoque t)
         {
             throw new NotImplementedException();
         }
 
-        public List<Consultar_produtos> List()
+        public List<Consultar_Estoque> List()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Consultar_Estoque> list = new List<Consultar_Estoque>();
+
+
+
+
+
+                return list;
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
-        public void Update(Consultar_produtos t)
+        public void Update(Consultar_Estoque t)
         {
             throw new NotImplementedException();
         }
