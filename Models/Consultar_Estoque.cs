f7b6d5b5ff0ced3dae4a18c43576aab8555b2ce3
Gabriel Henrique <ghlkoficial@gmail.com>
2021-10-15 23:04:04 -0300

    Consultar Estoque - Update
    Adicionado as informações para referência ao banco de dados ;)

--- Models/Consultar_Estoque.cs
+++ Models/Consultar_Estoque.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Siscream.Models
 {
-    class Consultar_produtos
+    class Consultar_Estoque
     {
+        public int Id { get; set; }
+       
+        public string Nome { get; set; }
+
+        public string unidademed_prod { get; set; }
+        public string datavalidade_prod { get; set; }
+        public string tipo_prod { get; set; }
+        public string estoque_prod { get; set; }
+        public string fabricante_prod { get; set; }
+        public string marca_prod { get; set; }
+        public string codbarras_prod { get; set; }
+        public string comissao_prod { get; set; }
+        public string preco_prod { get; set; }
+        public string custo_prod { get; set; }
+        public string descricao_prod { get; set; }
+
+
+         
     }
 }
