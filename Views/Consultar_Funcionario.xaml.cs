86b823718cef2306f1ee9c9c24db1c0317b94f70
Gabriel Henrique <ghlkoficial@gmail.com>
2021-10-27 10:28:01 -0300

    Consultar Funcionário

---
+++ Views/Consultar_Funcionario.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Siscream.Models;
+
+
+namespace Siscream.Views
+{
+    /// <summary>
+    /// Interação lógica para Consultar_Funcionario.xam
+    /// </summary>
+    public partial class Consultar_Funcionario : Window
+    {
+        public Consultar_Funcionario()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void btn_produtos_Click(object sender, RoutedEventArgs e)
+        {
+            SubMenu_Produtos produto = new SubMenu_Produtos();
+            produto.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_vendas_Click(object sender, RoutedEventArgs e)
+        {
+            SubMenu_Vendas submenu_vendas = new SubMenu_Vendas();
+            submenu_vendas.ShowDialog();
+        }
+
+        private void btn_gastos_Click(object sender, RoutedEventArgs e)
+        {
+            SubMenu_Gastos submenu_gastos = new SubMenu_Gastos();
+            submenu_gastos.ShowDialog();
+        }
+
+        private void btn_cadastros_Click(object sender, RoutedEventArgs e)
+        {
+            SubMenu_Cadastros submenu_cadastrar = new SubMenu_Cadastros();
+            submenu_cadastrar.ShowDialog();
+        }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bnt_consultar_funcionario_interno_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
