a919e7899d6358d788be6d7eb61855f04c4f55b9
Paulo Santos Silva <paulo252003@hotmail.com>
2021-10-26 21:19:25 -0400

    retirada de end_fk de funcionario

--- Models/Funcionario_DAO.cs
+++ Models/Funcionario_DAO.cs
@@ -57,7 +57,6 @@ namespace Siscream.Models
                     funcionario.Cargo = reader.GetString("cargo_func");
                     funcionario.Admissao = reader.GetDateTime("dataAdmissao_func");
                     funcionario.Contrato = reader.GetString("tipoContrato_func");
-                    funcionario.Codigo_end = reader.GetInt32("cod_end_fk");
                 }
 
 
